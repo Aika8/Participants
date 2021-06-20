a144370684da0c86779814ec6905567291968adf
Aika8 <aigerimt922@gmail.com>
2021-06-20 21:04:00 +0600

    little fixes

--- src/main/java/com/internship/second/controllers/HomeController.java
+++ src/main/java/com/internship/second/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.internship.second.controllers;
 
 import com.internship.second.model.Person;
+import com.internship.second.model.Phone;
 import com.internship.second.repository.PersonRepository;
 import com.internship.second.repository.PhoneRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/persons")
@@ -23,36 +26,36 @@ public class HomeController {
 
     @PostMapping()
     @ResponseStatus(value = HttpStatus.CREATED)
-    public ResponseEntity<?> save(@RequestBody Person person) {
+    public Person save(@RequestBody Person person) {
         log.info("create {}", person);
         person = personRepository.save(person);
-        return ResponseEntity.ok(person);
+        return person;
     }
 
 
     @GetMapping(value = "/by-ages")
-    public ResponseEntity<?> getBetweenAges(@RequestParam int age1, @RequestParam int age2) {
+    public List<Person> getBetweenAges(@RequestParam int age1, @RequestParam int age2) {
         log.info("get persons between ages {} and {}", age1, age2);
-        return ResponseEntity.ok(personRepository.findAllByAgeBetween(age1, age2));
+        return personRepository.findAllByAgeBetween(age1, age2);
     }
 
-    @GetMapping(value = "/by-name")
-    public ResponseEntity<?> getByName(@RequestParam String name) {
+    @GetMapping(value = "/by-name/{name}")
+    public List<Person> getByName(@PathVariable String name) {
         log.info("get persons by name containing {}", name);
-        return ResponseEntity.ok(personRepository.findAllByNameContaining(name));
+        return personRepository.findAllByNameContaining(name);
     }
 
     @GetMapping()
-    public ResponseEntity<?> get() {
+    public List<Person> get() {
         log.info("get all persons");
-        return ResponseEntity.ok(personRepository.findAll());
+        return personRepository.findAll();
     }
 
     @GetMapping(value = "/phones")
-    public ResponseEntity<?> getPhones(@RequestParam int id) {
+    public List<Phone> getPhones(@RequestParam int id) {
         Person person = personRepository.getById(id);
         log.info("get all phones of {}", person.getName());
-        return ResponseEntity.ok(phoneRepository.findAllByPerson(person));
+        return phoneRepository.findAllByPerson(person);
     }
 
 }
